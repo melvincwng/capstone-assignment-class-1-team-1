084f23b175e7387e38e071bf99a7d59a2f4ad832
melvincwng <melvin.ng@outlook.sg>
2023-01-14 00:18:16 +0800

    Setup model layer for express app

---
+++ server/model/databaseConfig.js
@@ -0,0 +1,13 @@
+var mysql = require("mysql");
+var dbconnect = {
+  getConnection: function () {
+    var conn = mysql.createConnection({
+      host: "localhost",
+      user: "root", // Change to your specific MySQL workbench username for it to work
+      password: "qwerty123", // Change to your specific MYSQL workbench password for it to work
+      database: "bdd_assignment",
+    });
+    return conn;
+  },
+};
+module.exports = dbconnect;
